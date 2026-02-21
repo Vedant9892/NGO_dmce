6ebf2f7708fc985a0c8c6737fe5f2930ddce0ac2
Hrushikesh Bhoir <rushikesh72@gmail.com>
2026-02-21 19:16:42 +0530

    Improved homepage design and backend updates

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,8 @@
 import 'dotenv/config';
 import { connectDB } from './config/db.js';
 import app from './app.js';
-
+import dotenv from 'dotenv';
+dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 connectDB()
